4a1b0742d0ae2a0da23d8c2c4b5f77709d9e57ca
nlin17 <155456184+nlin17@users.noreply.github.com>
2024-11-05 23:51:32 +0000

    wrote queue class

---
+++ queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__():
+        self.items = []
+
+    def enqueue(x):
+        self.items.append(x)
+
+    def dequeue():
+        return self.items.pop(0)
+
+    def display()
+    print(self.items)
+
+teddy = Queue()
+teddy.enqueue(1)
+teddy.enqueue(2)
+teddy.enqueue(3)
+teddy.enqueue(4)
+teddy.display()
+ben = teddy.dequeue()
+teddy.display()
+print(ben)
